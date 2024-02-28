ef3e751db84a24b6e0f2245558f01d8ee68d1aeb
nishant7002 <128689705+nishant7002@users.noreply.github.com>
2024-02-28 22:04:58 +0530

    p5(2).php

--- p5(2).php
+++ p5(2).php
@@ -16,7 +16,18 @@
     if (isset($_POST['calculate'])) {
         $array = $_POST['array'];
         $array = explode(',', $array);
-        sort($array);
+        
+        $n = count($array);
+        for ($i = 0; $i < $n; $i++) {
+            for ($j = 0; $j < $n - $i - 1; $j++) {
+                if ($array[$j] > $array[$j + 1]) {
+                    
+                    $temp = $array[$j];
+                    $array[$j] = $array[$j + 1];
+                    $array[$j + 1] = $temp;
+                }
+            }
+        }
         echo "<p>Sorted array:</p>";
         echo "<ul>";
         foreach ($array as $value) {
