d6278d708c711638002d34a308e42a503d381282
nishant7002 <128689705+nishant7002@users.noreply.github.com>
2024-03-07 21:27:58 +0530

    p4(2).php

--- p4(2).php
+++ p4(2).php
@@ -1,29 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GTU Grade Calculator</title>
+    <style>
+        body {
+            text-align: center;
+        }
+        form {
+            display: inline-block;
+            text-align: left;
+        }
+        table {
+            margin-left: auto;
+            margin-right: auto;
+        }
+    </style>
 </head>
 <body>
     <h1>GTU Grade Calculator</h1>
     <form action="" method="post">
-        <label for="sub1">Subject 1:</label>
-        <input type="number" id="sub1" name="sub1" min="0" max="100" required><br>
-        <label for="sub2">Subject 2:</label>
-        <input type="number" id="sub2" name="sub2" min="0" max="100" required><br>
-        <label for="sub3">Subject 3:</label>
-        <input type="number" id="sub3" name="sub3" min="0" max="100" required><br>
-        <label for="sub4">Subject 4:</label>
-        <input type="number" id="sub4" name="sub4" min="0" max="100" required><br>
+        <label for="Subject1">Subject 1:</label>
+        <input type="number" id="Subject1" name="Subject1" min="0" max="100" required><br>
+        <label for="Subject2">Subject 2:</label>
+        <input type="number" id="Subject2" name="Subject2" min="0" max="100" required><br>
+        <label for="Subject3">Subject 3:</label>
+        <input type="number" id="Subject3" name="Subject3" min="0" max="100" required><br>
+        <label for="Subject4">Subject 4:</label>
+        <input type="number" id="Subject4" name="Subject4" min="0" max="100" required><br>
         <button type="submit" name="calculate">Calculate</button>
     </form>
     <?php
     if (isset($_POST['calculate'])) {
-        $sub1 = $_POST['sub1'];
-        $sub2 = $_POST['sub2'];
-        $sub3 = $_POST['sub3'];
-        $sub4 = $_POST['sub4'];
+        $Subject1 = $_POST['Subject1'];
+        $Subject2 = $_POST['Subject2'];
+        $Subject3 = $_POST['Subject3'];
+        $Subject4 = $_POST['Subject4'];
 
         $grades = [
             'AA' => [85, 100],
@@ -37,24 +51,40 @@
         ];
 
         $results = [
-            'sub1' => 'FAIL',
-            'sub2' => 'FAIL',
-            'sub3' => 'FAIL',
-            'sub4' => 'FAIL'
+            'Subject1' => 'FAIL',
+            'Subject2' => 'FAIL',
+            'Subject3' => 'FAIL',
+            'Subject4' => 'FAIL'
         ];
 
+        $totalScore = 0;
+        $subjectsCount = 4;
+
         foreach ($grades as $grade => $range) {
-            if ($sub1 >= $range[0] && $sub1 <= $range[1]) {
-                $results['sub1'] = $grade;
+            if ($Subject1 >= $range[0] && $Subject1 <= $range[1]) {
+                $results['Subject1'] = $grade;
+                $totalScore += $Subject1;
             }
-            if ($sub2 >= $range[0] && $sub2 <= $range[1]) {
-                $results['sub2'] = $grade;
+            if ($Subject2 >= $range[0] && $Subject2 <= $range[1]) {
+                $results['Subject2'] = $grade;
+                $totalScore += $Subject2;
             }
-            if ($sub3 >= $range[0] && $sub3 <= $range[1]) {
-                $results['sub3'] = $grade;
+            if ($Subject3 >= $range[0] && $Subject3 <= $range[1]) {
+                $results['Subject3'] = $grade;
+                $totalScore += $Subject3;
             }
-            if ($sub4 >= $range[0] && $sub4 <= $range[1]) {
-                $results['sub4'] = $grade;
+            if ($Subject4 >= $range[0] && $Subject4 <= $range[1]) {
+                $results['Subject4'] = $grade;
+                $totalScore += $Subject4;
+            }
+        }
+
+        $averageScore = $totalScore / $subjectsCount;
+        $overallGrade = 'FAIL';
+        foreach ($grades as $grade => $range) {
+            if ($averageScore >= $range[0] && $averageScore <= $range[1]) {
+                $overallGrade = $grade;
+                break;
             }
         }
 
@@ -63,6 +93,7 @@
         foreach ($results as $subject => $grade) {
             echo "<tr><td>{$subject}</td><td>{$grade}</td></tr>";
         }
+        echo "<tr><td>Overall</td><td>{$overallGrade}</td></tr>";
         echo "</table>";
     }
     ?>
